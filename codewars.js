623b0b0d08da093c9deccea43567e111630d1cfa
claypilat <44934339+claypilat@users.noreply.github.com>
2019-06-25 09:14:21 +0930

    Commit added challenge

--- codewars.js
+++ codewars.js
@@ -57,3 +57,31 @@ function descendingOrder(n){
   }
 
   descendingOrder(596578)
+
+//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+// You have function with one side of the DNA (string, except for Haskell); 
+// you need to get the other complementary side. 
+// DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+  function DNAStrand(dna){
+    var splitString = (""+ dna).split("");
+    for(let i = 0; i < splitString.length; i++){
+        if(splitstring[i] == "A") {
+            splitstring[i].replace(/A/,"T");
+        }
+    }
+
+}
+  DNAStrand("ATTGC");
+// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+// years divisible by 4 are leap years
+// but years divisible by 100 are not leap years
+// but years divisible by 400 are leap years
+
+  function isLeapYear(year) {
+    // TODO
+
+
+
+    
+  }
